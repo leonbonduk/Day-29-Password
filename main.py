94ad6ec72113694407c6a5c76245bbd233124be3
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2021-12-16 21:22:31 +0000

    v3

--- main.py
+++ main.py
@@ -1,7 +1,65 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
 # ---------------------------- CONSTANTS ------------------------------- #
 WHITE = "#ffffff"
 FONT_NAME = "Courier"
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    #nr_letters = random.randint(8, 10)
+    #nr_symbols = random.randint(2, 4)
+    #nr_numbers = random.randint(2, 4)
+
+    #password_list = []
+
+    #for char in range(nr_letters):
+    #  password_list.append(random.choice(letters))
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+
+    #for char in range(nr_symbols):
+    #  password_list += random.choice(symbols)
+    password_symbols = [choice(symbols) for _ in range(randint(2,4))]
+
+    #for char in range(nr_numbers):
+    #  password_list += random.choice(numbers)
+    password_numbers = [choice(numbers) for _ in range(randint(2,4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+
+    #password = ""
+    #for char in password_list:
+    #  password += char
+
+    password = "".join(password_list)
+    print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) ==0 or len(password) ==0:
+        messagebox.showinfo(title="oops", message="Please make sure you haven't left any fields empty.")
+
+    is_ok = messagebox.askokcancel(title=website, message= f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+
+    if is_ok:
+        with open("data.txt", "a") as data_file:
+            #With will close file automatically
+            data_file.write(f" {website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -17,28 +75,30 @@ website_text = Label(text="Website:", bg=WHITE, font=(FONT_NAME, 12, "bold"))
 website_text.grid(column=0, row=1)
 
 #website_input = Text(height = 1, width = 35)
-website_input = Entry(width = 35)
-website_input.grid(column=1, row=1, columnspan =2)
+website_entry = Entry(width = 39)
+website_entry.grid(column=1, row=1, columnspan =2)
+website_entry.focus()
 
 email_text = Label(text="Email/Username:", bg=WHITE, font=(FONT_NAME, 12, "bold"))
 email_text.grid(column=0, row=2)
 
 #email_input = Text(height = 1, width = 35)
-email_input = Entry(width = 35)
-email_input.grid(column=1, row=2, columnspan =2)
+email_entry = Entry(width = 39)
+email_entry.grid(column=1, row=2, columnspan =2)
+email_entry.insert(END, "leonbond@hotmail.com")
 
 password_text = Label(text="Password:", bg=WHITE, font=(FONT_NAME, 12, "bold"))
 password_text.grid(column=0, row=3)
 
 #password_input = Text(height = 1, width = 21)
-password_input = Entry(width = 21)
-password_input.grid(column=1, row=3)
+password_entry = Entry(width = 21)
+password_entry.grid(column=1, row=3)
 
-password_button = Button(text="Generate Password", highlightthickness=0)
+password_button = Button(text="Generate Password", highlightthickness=0, bg=WHITE, command=generate_password)
 # password_button = Button(text="Generate Password", highlightthickness=0, command=generate_password)
 password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", highlightthickness=0, width = 36)
+add_button = Button(text="Add", highlightthickness=0, width = 33, bg=WHITE, command=save)
 add_button.grid(column=1, row=4, columnspan =2)
 
 
